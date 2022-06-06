38a4c8c32e5da3c64589f0bc81217e9630f84e7d
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2022-06-06 13:47:47 +0300

    name origin and env

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://profit.neurobica.online"],
+    origin: ["https://admin.neurobica.online"],
     credentials: true,
   })
 );
