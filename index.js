45a3605f0d239e140eca1543aadabf0556cf9f67
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-06-21 17:16:47 +0300

    erwf

--- index.js
+++ index.js
@@ -96,11 +96,8 @@ app.post("/login", async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        sameSite:
-          process.env.NODE_ENV === "development"
-            ? "lax"
-            : process.env.NODE_ENV === "production" && "none",
-        secure:
+        sameSite: process.env.NODE_ENV === "development" ? "lax" : " lax", // BECAUSE SAFARI
+        /*    : process.env.NODE_ENV === "production" && "none" */ secure:
           process.env.NODE_ENV === "development"
             ? false
             : process.env.NODE_ENV === "production" && true,
