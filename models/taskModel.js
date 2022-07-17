d541f89d6b455fffc204f9bb505dd75cb234ca0d
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-07-17 21:38:39 +0300

    q3e3qe

--- models/taskModel.js
+++ models/taskModel.js
@@ -5,7 +5,7 @@ const taskSchema = new mongoose.Schema(
     owner: { type: mongoose.Schema.Types.ObjectId, required: true },
     name: { type: String, required: true },
     desc: { type: String, required: true },
-    children: { type: Array, required: true },
+    parent: { type: mongoose.Schema.Types.ObjectId, required: false },
   },
   {
     timestamps: true,
