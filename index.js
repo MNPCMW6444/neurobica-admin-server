b5f8093b7ed19682609c4b2ef95afe717e706f60
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-07-29 12:27:32 +0300

    Revert "Update index.js"
    This reverts commit 4241f0aee741d4e99e44132f80cc8fe781e37752.

--- index.js
+++ index.js
@@ -338,11 +338,14 @@ app.post("/publish", async (req, res) => {
       "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"
     );
     const msg = {
-      to: "founders@neurobica.online", // Change to your recipient
+      to: "yoad12121223@gmail.com", // Change to your recipient
       from: "service@neurobica.online", // Change to your verified sender
-      subject: "A new publicaition was made by " + userr.name,
+      subject: "A new publicaition was made by " + validatedUser.name,
       text: desc,
-      html: "<h1>The Content:</h1><p>" + desc + "</p>",
+      html:
+        "<h1>this is h1</h1><p>this is p and the publication is:</p><p>" +
+        desc +
+        "</p>",
     };
     sgMail
       .send(msg)
